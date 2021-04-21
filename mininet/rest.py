cb5030c3066f284f6851d9021fda0221d699e1b6
Will Fantom <willfantom@gmail.com>
2021-04-21 12:50:14 +0100

    Improve node process management over the API (#5)
    * Install bottle rest api framework
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Initial commit of integrated rest api
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Add support for rest to mn
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Added iperf and node info endpoints to api
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Added custom command endpoint
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Use popen to run node processes
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Renamed response fields for exporter
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Support more Iperf config options
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * Improved proc mgmt over rest
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>
    
    * General API cleanup 🧹
    
    Signed-off-by: Will Fantom <willfantom@gmail.com>

--- mininet/rest.py
+++ mininet/rest.py
@@ -37,14 +37,16 @@ from mininet.log import error as mnerror
 class REST(Bottle):
     "Simple REST API to talk to nodes."
 
-    prefix = '/mn/api'
-
     def __init__(self, mininet, port=8080, **kwargs):
         """Start and run interactive REST API
            mininet: Mininet network object
            port: port to expose the API"""
         super(REST, self).__init__()
         self.mn = mininet
+        self.prefix = kwargs.get("prefix", "/mn/api")
+        if not self.prefix.startswith('/'):
+            mnerror('the api prefix must start with a /\n')
+            return
         if port > 65536 or port < 1:
             mnerror('given port value is not valid for the api\n')
             return
@@ -60,13 +62,23 @@ class REST(Bottle):
         self.route(self.prefix+'/stop', callback=self.__stop)
         self.route(self.prefix+'/pingset', callback=self.__pingSet)
         self.route(self.prefix+'/pingall', callback=self.__pingAll)
+
         self.route(self.prefix+'/iperfpair', callback=self.__iperfPair)
+        # Create a new command
         self.route(self.prefix+'/node/<node_name>/process/create',
-                   method='POST', callback=self.__nodePOpen)
+                   method='POST', callback=self.__nodeCmd)
+        # Get command list
+        self.route(self.prefix+'/node/<node_name>/processes',
+                   callback=self.__nodeCmdList)
+        self.route(self.prefix+'/node/<node_name>/process/list',
+                   callback=self.__nodeCmdList)
+
         self.route(self.prefix+'/node/<node_name>/process/<pid>',
-                   callback=self.__nodePOpenMonitor)
+                   callback=self.__nodeCmdMonitor)
         self.route(self.prefix+'/node/<node_name>/process/<pid>/terminate',
-                   callback=self.__nodePOpenTerminate)
+                   callback=self.__nodeCmdTerminate)
+        self.route(self.prefix+'/node/<node_name>/process/<pid>/remove',
+                   callback=self.__nodeCmdRemove)
 
         try:
             self.run(host='0.0.0.0', port=port, quiet=True)
@@ -110,7 +122,23 @@ class REST(Bottle):
 
     def __getNodes(self):
         """Get a list of nodes that exist within a topo"""
-        data = {"nodes": [node for node in self.mn]}
+        full = request.query.get("full", False)
+        try:
+            full = bool(full)
+        except:
+            data = {"error": "full should be true/false"}
+            return self.__build_response(data, code=400)
+        if not full:
+            data = {"nodes": [node for node in self.mn]}
+        else:
+            data = {
+                "nodes": [
+                    {
+                        "name": self.mn.get(n).name,
+                        "class": self.mn.get(n).__class__.__name__
+                    } for n in self.mn
+                ]
+            }
         return self.__build_response(data)
 
     def __getNode(self, node_name):
@@ -137,27 +165,47 @@ class REST(Bottle):
         return self.__returnPingData(results)
 
     def __iperfPair(self):
-        hosts, error = self.__getHostSet(request.query.hosts)
-        if not hosts:
-            data = {"error": error}
+        """ Run an Iperf test bettween 2 hosts and return the results"""
+        if str(request.query.get("server", None)) in self.mn:
+            server = self.mn.get(str(request.query.server))
+        else:
+            data = {"error": "server not a valid host"}
             return self.__build_response(data, code=400)
-        if len(hosts) > 2:
-            data = {"error": "iperf only supports a set of 2 hosts"}
+        if str(request.query.get("client", None)) in self.mn:
+            client = self.mn.get(str(request.query.client))
+        else:
+            data = {"error": "client not a valid host"}
+            return self.__build_response(data, code=400)
+        type_ = str(request.query.get("type", "TCP")).upper()
+        if not type_ in ["UDP", "TCP"]:
+            data = {"error": "type must be TCP or UDP"}
+            return self.__build_response(data, code=400)
+        time = request.query.get("time", 5)
+        try:
+            time = int(time)
+        except:
+            data = {"error": "time should be a number"}
+            return self.__build_response(data, code=400)
+        if time <= 0 or time >= 31:
+            data = {"error": "time should be between 1 and 30"}
             return self.__build_response(data, code=400)
         try:
-            results = self.mn.iperf(hosts=hosts)
+            results = self.mn.iperf(
+                hosts=[client, server], seconds=time, fmt='m', l4Type=type_)
         except:
             data = {"error": "iperf failed"}
             return self.__build_response(data, code=500)
         data = {
-            "server": hosts[1].name,
-            "client": hosts[0].name,
+            "server": server.name,
+            "client": client.name,
+            "traffic_type": type_,
+            "seconds": time,
             "client_speed": results[1],
             "server_speed": results[0]
         }
         return self.__build_response(data)
 
-    def __nodePOpen(self, node_name):
+    def __nodeCmd(self, node_name):
         if not node_name in self.mn:
             data = {"error": "node does not exist"}
             return self.__build_response(data, code=400)
@@ -176,44 +224,95 @@ class REST(Bottle):
         try:
             popen = node.popen(body.get("command"),
                                stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            self.node_procs.setdefault(node_name, {})
-            self.node_procs[node_name][str(popen.pid)] = popen
+            self.node_procs.setdefault(node_name, [])
+            self.node_procs[node_name].append(
+                {
+                    "process": popen,
+                    "command": body.get("command"),
+                    "exit_code": None,
+                    "pid": popen.pid,
+                    "stdout": "",
+                    "stderr": ""
+                }
+            )
             data = {
-                "success": ("created process pid:%s" % str(popen.pid))
+                "success": "created process pid",
+                "pid": popen.pid
             }
             return self.__build_response(data, code=200)
         except:
             data = {"error": "popen command failed"}
             return self.__build_response(data, code=500)
 
-    def __nodePOpenMonitor(self, node_name, pid):
-        if not self.node_procs.get(node_name, {}).get(pid, None):
-            data = {"error": "process does not exist"}
-            return self.__build_response(data, code=400)
-        proc = (self.node_procs[node_name][pid])
-        exit_code = proc.poll()
-        if not exit_code == None:
-            data = {
-                "node_name": node_name,
-                "status": "stopped",
-                "exit_code": exit_code,
-                "stdout": codecs.decode(proc.stdout.read()),
-                "stderr": codecs.decode(proc.stderr.read())
+    def __nodeCmdList(self, node_name):
+        if node_name in self.node_procs:
+            procs = {
+                "processes": [{
+                    "pid": p.get("pid", 0),
+                    "state": ("running" if p.get("process").poll() == None else "stopped"),
+                    "command": p.get("command", "nil")
+                } for p in self.node_procs.get(node_name)]
             }
-            (self.node_procs[node_name]).pop(pid)
-        else:
-            data = {"node_name": node_name, "status": "running"}
-        return self.__build_response(data, code=200)
+            return self.__build_response(procs, code=200)
+        data = {"error": "node command list can not be found"}
+        return self.__build_response(data, code=400)
 
-    def __nodePOpenTerminate(self, node_name, pid):
-        if not self.node_procs.get(node_name, {}).get(pid, None):
-            data = {"error": "process does not exist"}
+    def __nodeCmdMonitor(self, node_name, pid):
+        if not node_name in self.node_procs:
+            data = {"error": "node processes can not be found"}
             return self.__build_response(data, code=400)
-        proc = (self.node_procs[node_name][pid])
-        exit_code = proc.poll()
-        if exit_code == None:
-            proc.terminate()
-            data = {"success": "process termination signal sent"}
-        else:
-            data = {"success": "process has already terminated"}
-        return self.__build_response(data, code=200)
+        for proc in self.node_procs.get(node_name, []):
+            if str(proc.get("pid")) == pid:
+                popen = proc.get("process")
+                proc["exit_code"] = popen.poll()
+                try:
+                    out, err = popen.communicate(timeout=5)
+                    proc["stdout"] = out.decode('utf-8', 'backslashreplace')
+                    proc["stderr"] = err.decode('utf-8', 'backslashreplace')
+                except:
+                    mnerror('Attempted to read streams of non-terminated process\n')
+                data = {
+                    "pid": popen.pid,
+                    "state": ("running" if proc.get("process").poll() == None else "stopped"),
+                    "exit_code": proc.get("process").poll(),
+                    "stdout": proc["stdout"],
+                    "stderr": proc["stderr"],
+                    "command": proc.get("command")
+                }
+                return self.__build_response(data, code=200)
+        data = {"error": "process with given id can not be found"}
+        return self.__build_response(data, code=400)
+
+    def __nodeCmdTerminate(self, node_name, pid):
+        if not node_name in self.node_procs:
+            data = {"error": "node processes can not be found"}
+            return self.__build_response(data, code=400)
+        for proc in self.node_procs.get(node_name, []):
+            if str(proc.get("pid")) == pid:
+                popen = proc.get("process")
+                proc["exit_code"] = popen.poll()
+                if proc["exit_code"] == None:
+                    popen.terminate()
+                    data = {"success": "process termination signal sent"}
+                else:
+                    data = {"success": "process is already terminated"}
+                return self.__build_response(data, code=200)
+        data = {"error": "process could not be found"}
+        return self.__build_response(data, code=400)
+
+    def __nodeCmdRemove(self, node_name, pid):
+        if not node_name in self.node_procs:
+            data = {"error": "node processes can not be found"}
+            return self.__build_response(data, code=400)
+        for proc in self.node_procs.get(node_name, []):
+            if str(proc.get("pid")) == pid:
+                popen = proc.get("process")
+                proc["exit_code"] = popen.poll()
+                if proc["exit_code"] == None:
+                    data = {"error": "process must be terminated to remove"}
+                else:
+                    self.node_procs.get(node_name).remove(proc)
+                    data = {"success": "process removed"}
+                return self.__build_response(data, code=200)
+        data = {"error": "process could not be found"}
+        return self.__build_response(data, code=400)
